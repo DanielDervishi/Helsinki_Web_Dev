38976efbec9d9b4d74eb276f1c5dfdd5c48fef0b
DanielDervishi <58835213+DanielDervishi@users.noreply.github.com>
2022-05-12 12:37:08 -0400

    Part 1 - Exercises Completed

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,15 +10,33 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0])
   const [selected, setSelected] = useState(0)
+  const [mostVotes, setMostVotes] = useState(0)
+
+  const updateVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+    if (newVotes[selected] > newVotes[mostVotes]) {
+      setMostVotes(selected)
+    }
+  }
 
   return (
     <>
+      <h1>Anecdote of the day</h1>
       <div>
         {anecdotes[selected]}
       </div>
+      <div>
+        has {votes[selected]} votes
+      </div>
+      <button onClick={updateVote}>vote</button>
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anicdote</button>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[mostVotes]}</div>
+      <div>has {votes[mostVotes]} votes</div>
     </>
   )
 }
