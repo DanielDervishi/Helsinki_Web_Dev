eb235bad91484fb7c90af77a3e0b507916b974f5
DanielDervishi <58835213+DanielDervishi@users.noreply.github.com>
2022-05-17 15:08:26 -0400

    Part 3(a) - Exercises Complete

--- part3/PhoneBook/index.js
+++ part3/PhoneBook/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
+morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 let persons = [
     {
         "id": 1,
