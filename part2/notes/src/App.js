acef64c633ca10cba01b17f9efc21bf4288cba5d
DanielDervishi <58835213+DanielDervishi@users.noreply.github.com>
2022-05-20 13:18:35 -0400

    Website Deployed

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
-const baseURL = 'http://localhost:3001/api/notes'
+const baseURL = '/api/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
