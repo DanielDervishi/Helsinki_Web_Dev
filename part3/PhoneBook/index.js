da07185cd99174594cc7c54e2f2bfcab378ca320
DanielDervishi <58835213+DanielDervishi@users.noreply.github.com>
2022-05-30 10:29:11 -0400

    Added process.env.PORT to assure functionality on heroku servers

--- part3/PhoneBook/index.js
+++ part3/PhoneBook/index.js
@@ -77,7 +77,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
