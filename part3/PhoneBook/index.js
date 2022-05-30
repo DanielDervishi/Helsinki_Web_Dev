068c1848ede63f9c9d21ca03975a9c8b7906111f
DanielDervishi <58835213+DanielDervishi@users.noreply.github.com>
2022-05-30 11:23:53 -0400

    Part 3(b) - Completed All Exercises

--- part3/PhoneBook/index.js
+++ part3/PhoneBook/index.js
@@ -3,10 +3,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
-
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
+morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
